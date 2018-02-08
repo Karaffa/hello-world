1543055d49967ff4c35a35fb8d631e9f72510550
Pierre Karaffa <35781401+Karaffa@users.noreply.github.com>
2018-02-08 16:27:25 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,76 +2,11 @@
 #include "frameGenerator.h"
 #include <string>
 #include <iostream>
+#include "ShapeGenerator.h"
 
 const std::string NAME = "pkaraff";
 const int WIDTH = 640;
 const int HEIGHT = 480;
-
-class ShapeGenerator {
-public:
-  ShapeGenerator(SDL_Color, SDL_Point, SDL_Rect);
-  void drawCircle(SDL_Renderer* renderer, int radius);
-  void drawRectangle(SDL_Renderer* renderer);
-  void drawGradientCircle(SDL_Renderer* renderer, int radius, char hue);
-  void setColor(SDL_Color);
-  void setCenter(SDL_Point);
-  void setCorner(SDL_Rect);
-  const std::string overloaded();
-private:
-  SDL_Color color;
-  SDL_Point center;
-  SDL_Rect corner;
-};
-
-ShapeGenerator::ShapeGenerator(SDL_Color col, SDL_Point cen, SDL_Rect cor) : 
-  color(col), center(cen), corner(cor) {}
-
-void ShapeGenerator::drawCircle(SDL_Renderer* renderer, int radius) {
-  SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
-  for (int w = 0; w < radius * 2; w++) {
-    for (int h = 0; h < radius * 2; h++) {
-      int dx = radius - w; // horizontal offset
-      int dy = radius - h; // vertical offset
-      if ((dx*dx + dy*dy) <= (radius * radius)) {
-        SDL_RenderDrawPoint(renderer, center.x + dx, center.y + dy);
-      }
-    }
-  }
-}
-
-void ShapeGenerator::drawRectangle(SDL_Renderer* renderer){
-  SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
-  SDL_RenderFillRect(renderer, &corner);
-}
-
-void ShapeGenerator::drawGradientCircle(SDL_Renderer* renderer, int radius, char hue){
-  for(int i = radius; i > 0; i--){
-    if(hue == 'r') color.r++;
-    if(hue == 'g') color.g++;
-    if(hue == 'b') color.b++;
-    if(hue == 'y') {color.r++; color.g++;}
-    if(hue == 'p') {color.r+=2; color.b+=2;}
-    if(hue == 'o') {color.r+=2; color.g++;}
-    drawCircle(renderer, i);
-  }
-}
-
-void ShapeGenerator::setColor(SDL_Color col){
-  color = col;
-}
-
-void ShapeGenerator::setCenter(SDL_Point cen){
-  center = cen;
-}
-
-void ShapeGenerator::setCorner(SDL_Rect cor){
-  corner = cor;
-}
-
-const std::string ShapeGenerator::overloaded() {
-  return "This operator is overloaded";
-}
-
 
 std::ostream& operator<<(std::ostream& out, ShapeGenerator Generate) {
   return out << Generate.overloaded();
